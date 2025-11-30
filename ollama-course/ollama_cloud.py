c3e8646f068db24d0ab5a23e5b2a048bf79c6d0b
tylerreed <tylerreedytlearning@gmail.com>
2025-11-30 11:04:19 -0500

    amazing cloud usage with this simple code

---
+++ ollama-course/ollama_cloud.py
@@ -0,0 +1,13 @@
+from ollama import Client
+
+client = Client()
+
+messages = [
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+]
+
+for part in client.chat('gpt-oss:120b-cloud', messages=messages, stream=True):
+  print(part['message']['content'], end='', flush=True)
